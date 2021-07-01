02616bfc96ab062980ee3760a3c63fec76f5670a
Shravan Asati <dev.shravan@protonmail.com>
2021-07-01 12:29:29 +0530

    ✅ remove dummy oauth code, edited gitignore, LICENSE and main.py

--- chronocli/__init__.py
+++ chronocli/__init__.py
@@ -1,1 +1,5 @@
+"""
+Chrono has been licensed under GNU GPL 3. View the LICENSE file for more details.
+"""
+
 __version__ = '0.1.0'
--- chronocli/main.py
+++ chronocli/main.py
@@ -1,29 +1,33 @@
 import click
+
+
 @click.group()
-@click.version_option('0.2.3', prog_name='chrono')
+@click.version_option('0.1.0', prog_name='chrono')
 def main():
-    '''Submit your weekly challenges solutions'''
+    """
+    Submit your weekly challenges solutions using chrono.
+    """
     pass
+
 
 @main.command('submit', help='Submit your solution')
 @click.argument('filename', nargs=1)
-def submit(filename):
-    ext = filename.split('.')
-    ext =repr(ext[-1])
-    if ext == "'py'":
+def submit(filename: str) -> None:
+    ext = filename.split('.')[-1].strip()
+
+    if ext == "py":
         lang = 'python'
-    elif ext == "'js'":
+    elif ext == "js":
         lang = 'javascript'
     else:
         click.echo('Invalid File Format')
-        exit()
+        return
 
     try:
         with open(filename, 'r') as f:
             code = f.read()
     except Exception as e:
         pass
-
 
 
 if __name__ == "__main__":
